ccfaa284d9fff6c3fb177fae2ae897a9873580ba
João Afonso Katombela <ja3328173@gmail.com>
2024-05-05 16:43:05 +0100

    fix web cache

--- app-multiple-account.js
+++ app-multiple-account.js
@@ -81,6 +81,12 @@ const createSession = function(id, description) {
         '--disable-gpu'
       ],
     },
+    
+  webVersionCache: {
+    type: 'remote',
+    remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
+    },
+    
     authStrategy: new LocalAuth({
       clientId: id
     })
