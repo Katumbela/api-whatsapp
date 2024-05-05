08d461a6785e16ea66d71035475a2f6d18293fbe
João Afonso Katombela <ja3328173@gmail.com>
2024-05-05 13:27:42 +0100

    add client

--- app.js
+++ app.js
@@ -53,6 +53,10 @@ const client = new Client({
       '--disable-gpu'
     ],
   },
+  webVersionCache: {
+    type: 'remote',
+    remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
+    },
   authStrategy: new LocalAuth()
 });
 
