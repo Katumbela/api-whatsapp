455e54d3e814890dbff8ab0b5e6b306cc0b3102a
João Afonso Katombela <ja3328173@gmail.com>
2024-05-05 23:42:56 +0100

    backup app.js

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
-const { Client, MessageMedia, LocalAuth } = require('whatsapp-web.js');
+const { Client, MessageMedia , LocalAuth } = require('whatsapp-web.js');
 const express = require('express');
-const { body, validationResult } = require('express-validator');
 const socketIO = require('socket.io');
 const qrcode = require('qrcode');
 const http = require('http');
-const fs = require('fs');
+const mongoose = require('mongoose');
 const { phoneNumberFormatter } = require('./helpers/formatter');
 const fileUpload = require('express-fileupload');
 const axios = require('axios');
-const mime = require('mime-types');
-
-const port = process.env.PORT || 8000;
+require('dotenv').config();
 
 const app = express();
 const server = http.createServer(app);
@@ -21,171 +18,273 @@ app.use(express.urlencoded({
   extended: true
 }));
 
-/**
- * BASED ON MANY QUESTIONS
- * Actually ready mentioned on the tutorials
- * 
- * Many people confused about the warning for file-upload
- * So, we just disabling the debug for simplicity.
- */
 app.use(fileUpload({
   debug: false
 }));
 
 app.get('/', (req, res) => {
-  res.sendFile('index.html', {
+  res.sendFile('index-multiple-account.html', {
     root: __dirname
   });
 });
 
-const client = new Client({
-  restartOnAuthFail: true,
-  puppeteer: {
-    headless: true,
-    args: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-      '--disable-dev-shm-usage',
-      '--disable-accelerated-2d-canvas',
-      '--no-first-run',
-      '--no-zygote',
-      '--single-process', // <- this one doesn't works in Windows
-      '--disable-gpu'
-    ],
-  },
-  webVersionCache: {
-    type: 'remote',
-    remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
-    },
-  authStrategy: new LocalAuth()
+// MongoDB connection
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Error connecting to MongoDB:', err));
+
+// Define o esquema do modelo de sessão
+const sessionSchema = new mongoose.Schema({
+  id: String,
+  description: String,
+  ready: Boolean,
+  // Outros campos necessários
 });
 
-client.on('message', msg => {
-  if (msg.body == '!ping') {
-    msg.reply('pong');
-  } else if (msg.body == 'good morning') {
-    msg.reply('selamat pagi');
-  } else if (msg.body == '!groups') {
-    client.getChats().then(chats => {
-      const groups = chats.filter(chat => chat.isGroup);
+// Cria o modelo de sessão
+const Session = mongoose.model('Session', sessionSchema);
 
-      if (groups.length == 0) {
-        msg.reply('You have no group yet.');
-      } else {
-        let replyMsg = '*YOUR GROUPS*\n\n';
-        groups.forEach((group, i) => {
-          replyMsg += `ID: ${group.id._serialized}\nName: ${group.name}\n\n`;
-        });
-        replyMsg += '_You can use the group id to send a message to the group._'
-        msg.reply(replyMsg);
-      }
-    });
+// Função para criar uma nova sessão e salvá-la no MongoDB
+const createSession = async function (id, description) {
+  console.log('Creating session: ' + id);
+  
+  const session = new Session({
+    id: id,
+    description: description,
+    ready: false,
+    // Outros campos necessários
+  });
+
+  try {
+    await session.save();
+    console.log('Session created and saved to MongoDB');
+  } catch (err) {
+    console.error('Error saving session to MongoDB:', err);
   }
 
-  // NOTE!
-  // UNCOMMENT THE SCRIPT BELOW IF YOU WANT TO SAVE THE MESSAGE MEDIA FILES
-  // Downloading media
-  // if (msg.hasMedia) {
-  //   msg.downloadMedia().then(media => {
-  //     // To better understanding
-  //     // Please look at the console what data we get
-  //     console.log(media);
+  const client = new Client({
+    restartOnAuthFail: true,
+    puppeteer: {
+      headless: true,
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-accelerated-2d-canvas',
+        '--no-first-run',
+        '--no-zygote',
+        '--single-process', // <- this one doesn't works in Windows
+        '--disable-gpu'
+      ],
+    },
 
-  //     if (media) {
-  //       // The folder to store: change as you want!
-  //       // Create if not exists
-  //       const mediaPath = './downloaded-media/';
+    webVersionCache: {
+      type: 'remote',
+      remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
+    },
 
-  //       if (!fs.existsSync(mediaPath)) {
-  //         fs.mkdirSync(mediaPath);
-  //       }
+    authStrategy: new LocalAuth({
+      clientId: id
+    })
+  });
 
-  //       // Get the file extension by mime-type
-  //       const extension = mime.extension(media.mimetype);
-        
-  //       // Filename: change as you want! 
-  //       // I will use the time for this example
-  //       // Why not use media.filename? Because the value is not certain exists
-  //       const filename = new Date().getTime();
-
-  //       const fullFilename = mediaPath + filename + '.' + extension;
-
-  //       // Save to file
-  //       try {
-  //         fs.writeFileSync(fullFilename, media.data, { encoding: 'base64' }); 
-  //         console.log('File downloaded successfully!', fullFilename);
-  //       } catch (err) {
-  //         console.log('Failed to save the file:', err);
-  //       }
-  //     }
-  //   });
-  // }
-});
-
-client.initialize();
-
-// Socket IO
-io.on('connection', function(socket) {
-  socket.emit('message', 'Conectando...');
+  client.on('message', msg => {
+    io.emit('message', { id: id, text: msg.body });
+  
+    if (msg.body == '!ping') {
+      msg.reply('pong');
+      client.sendMessage(msg.from, 'Esta é uma mensagem automática Reputação 360');
+    } else if (msg.body == 'bom dia !') {
+      msg.reply('Bom dia, como está?!');
+      client.sendMessage(msg.from, 'Esta é uma mensagem automática Reputação 360');
+    } else if (msg.body == 'tou bem !') {
+      msg.reply('Que bom que está bem, como posso ajudar?!');
+      client.sendMessage(msg.from, 'Esta é uma mensagem automática Reputação 360');
+    } else if (msg.body == '!atendimento') {
+      const atendimentoOptions = {
+        message: 'Selecione uma opção de atendimento:',
+        action: 'Selecionar',
+        options: [
+          { title: 'Consulta' },
+          { title: 'Suporte Técnico' },
+          { title: 'Cancelamento de Serviço' },
+          { title: 'Ver Website' }
+        ]
+      };
+      msg.reply(atendimentoOptions.message);
+      atendimentoOptions.options.forEach((option, index) => {
+        msg.reply(`${index + 1}: ${option.title}`);
+      });
+    } else if (msg.body == '1') {
+      msg.reply('Você selecionou Consulta. Por favor, forneça mais detalhes sobre sua consulta.');
+    } else if (msg.body == '2') {
+      msg.reply('Você selecionou Suporte Técnico. Como posso ajudá-lo com o suporte técnico?');
+    } else if (msg.body == '3') {
+      msg.reply('Você selecionou Cancelamento de Serviço. Por favor, entre em contato conosco para mais informações sobre o cancelamento.');
+    } else if (msg.body == '4') {
+      msg.reply('Aqui está o link para o website da Reputação 360: https://reputacao360.com');
+    } else if (msg.body == '!groups') {
+      client.getChats().then(chats => {
+        if (chats.length == 0) {
+          msg.reply('You have no group yet.');
+        } else {
+          let replyMsg = '*YOUR GROUPS*\n\n';
+          chats.forEach((group, i) => {
+            replyMsg += `ID: ${group.id}\nType: ${group.isGroup}\nLastMsg: ${group.lastMessage}\nTime: ${group.timestamp}\nName: ${group.name}\n\n`;
+          });
+          replyMsg += '_You can use the group id to send a message to the group._'
+          msg.reply(replyMsg);
+          client.sendMessage(msg.from, replyMsg);
+          io.emit('chats', { id: id, text: replyMsg });
+        }
+      });
+    }
+  });
+  
 
   client.on('qr', (qr) => {
-    console.log('RECEBENDO QRCODE', qr);
+    console.log('QR RECEIVED', qr);
     qrcode.toDataURL(qr, (err, url) => {
-      socket.emit('qr', url);
-      socket.emit('message', 'QRCODE RECEBIDO, SCANEIE PARA AUTENTICAR!');
+      io.emit('qr', { id: id, src: url });
+      io.emit('message', { id: id, text: 'QR Code received, scan please!' });
     });
   });
 
-  client.on('ready', () => {
-    socket.emit('ready', 'Whatsapp Conectado!');
-    socket.emit('message', 'Whatsapp Conectado!');
+  client.on('ready', async () => {
+    io.emit('ready', { id: id });
+    io.emit('message', { id: id, text: 'Whatsapp is ready!' });
+
+    const savedSessions = await getSessionsFromMongoDB();
+    const sessionIndex = savedSessions.findIndex(sess => sess.id == id);
+    savedSessions[sessionIndex].ready = true;
+    await updateSessionInMongoDB(id, { ready: true });
+ 
+    client.getChats().then(chats => {
+      //const groups = chats.filter(chat => chat.isGroup);
+
+      if (chats.length == 0) {
+        
+      } else {
+        let replyMsg = '*YOUR GROUPS*\n\n';
+        chats.forEach((group, i) => {
+          replyMsg += group;
+          replyMsg += `ID: ${group.id}\n\nType: ${group.isGroup}LastMsg: ${group.lastMessage}\n\nTime: ${group.timestamp}\n\nName: ${group.name}\n\n`;
+        });
+        replyMsg += '_You can use the group id to send a message to the group._'
+        
+        io.emit('chats', { id: id, text: replyMsg });
+      }
+    });
   });
 
   client.on('authenticated', () => {
-    socket.emit('authenticated', 'Whatsapp está autenticado!');
-    socket.emit('message', 'Whatsapp está autenticado!');
-    console.log('AUTHENTICATED');
+    io.emit('authenticated', { id: id });
+    io.emit('message', { id: id, text: 'Whatsapp is authenticated!' });
   });
 
-  client.on('auth_failure', function(session) {
-    socket.emit('message', 'Auth failure, restarting...');
+  client.on('auth_failure', function () {
+    io.emit('message', { id: id, text: 'Auth failure, restarting...' });
   });
 
   client.on('disconnected', (reason) => {
-    socket.emit('message', 'Whatsapp está desconectado!');
+    io.emit('message', { id: id, text: 'Whatsapp is disconnected! because:' + reason });
     client.destroy();
     client.initialize();
-  });
-});
 
-
-const checkRegisteredNumber = async function(number) {
-  const isRegistered = await client.isRegisteredUser(number);
-  return isRegistered;
-}
-
-// Send message
-app.post('/send-message', [
-  body('number').notEmpty(),
-  body('message').notEmpty(),
-], async (req, res) => {
-  const errors = validationResult(req).formatWith(({
-    msg
-  }) => {
-    return msg;
+    // Menghapus pada file sessions
+    const savedSessions = await getSessionsFromMongoDB();
+    const sessionIndex = savedSessions.findIndex(sess => sess.id == id);
+    savedSessions.splice(sessionIndex, 1);
+    await Session.deleteOne({ id: id });
+    io.emit('remove-session', id);
   });
 
-  if (!errors.isEmpty()) {
-    return res.status(422).json({
-      status: false,
-      message: errors.mapped()
+  // Tambahkan client ke sessions
+  sessions.push({
+    id: id,
+    description: description,
+    client: client
+  });
+
+  // Menambahkan session ke file
+  const savedSessions = await getSessionsFromMongoDB();
+  const sessionIndex = savedSessions.findIndex(sess => sess.id == id);
+
+  if (sessionIndex == -1) {
+
+    savedSessions.push({
+      id: id,
+      description: description,
+      ready: false,
+    });
+    await Session.create({
+      id: id,
+      description: description,
+      ready: false,
     });
   }
 
+  
+  client.initialize();
+}
+
+// Função de inicialização
+const init = async function (socket) {
+  const savedSessions = await getSessionsFromMongoDB();
+
+  if (savedSessions.length > 0) {
+    if (socket) {
+      savedSessions.forEach((e, i, arr) => {
+        arr[i].ready = false;
+      });
+
+      socket.emit('init', savedSessions);
+    } else {
+      savedSessions.forEach(sess => {
+        createSession(sess.id, sess.description);
+      });
+    }
+  }
+}
+
+// Restante do seu código...
+
+// Socket IO
+io.on('connection', function (socket) {
+  init(socket);
+
+  socket.on('create-session', function (data) {
+    console.log('Create session: ' + data.id);
+    createSession(data.id, data.description);
+  });
+});
+
+// Send message
+app.post('/send-message', async (req, res) => {
+  console.log(req);
+
+  const sender = req.body.sender;
   const number = phoneNumberFormatter(req.body.number);
   const message = req.body.message;
 
-  const isRegisteredNumber = await checkRegisteredNumber(number);
+  const client = sessions.find(sess => sess.id == sender)?.client;
+
+  // Make sure the sender is exists & ready
+  if (!client) {
+    return res.status(422).json({
+      status: false,
+      message: `The sender: ${sender} is not found!`
+    })
+  }
+
+  /**
+   * Check if the number is already registered
+   * Copied from app.js
+   * 
+   * Please check app.js for more validations example
+   * You can add the same here!
+   */
+  const isRegisteredNumber = await client.isRegisteredUser(number);
 
   if (!isRegisteredNumber) {
     return res.status(422).json({
@@ -207,145 +306,8 @@ app.post('/send-message', [
   });
 });
 
-// Send media
-app.post('/send-media', async (req, res) => {
-  const number = phoneNumberFormatter(req.body.number);
-  const caption = req.body.caption;
-  const fileUrl = req.body.file;
-
-  // const media = MessageMedia.fromFilePath('./image-example.png');
-  // const file = req.files.file;
-  // const media = new MessageMedia(file.mimetype, file.data.toString('base64'), file.name);
-  let mimetype;
-  const attachment = await axios.get(fileUrl, {
-    responseType: 'arraybuffer'
-  }).then(response => {
-    mimetype = response.headers['content-type'];
-    return response.data.toString('base64');
-  });
-
-  const media = new MessageMedia(mimetype, attachment, 'Media');
-
-  client.sendMessage(number, media, {
-    caption: caption
-  }).then(response => {
-    res.status(200).json({
-      status: true,
-      response: response
-    });
-  }).catch(err => {
-    res.status(500).json({
-      status: false,
-      response: err
-    });
-  });
+server.listen(process.env.PORT || 8000, function () {
+  console.log('App running on *: ' + (process.env.PORT || 8000));
 });
 
-const findGroupByName = async function(name) {
-  const group = await client.getChats().then(chats => {
-    return chats.find(chat => 
-      chat.isGroup && chat.name.toLowerCase() == name.toLowerCase()
-    );
-  });
-  return group;
-}
-
-// Send message to group
-// You can use chatID or group name, yea!
-app.post('/send-group-message', [
-  body('id').custom((value, { req }) => {
-    if (!value && !req.body.name) {
-      throw new Error('Invalid value, you can use `id` or `name`');
-    }
-    return true;
-  }),
-  body('message').notEmpty(),
-], async (req, res) => {
-  const errors = validationResult(req).formatWith(({
-    msg
-  }) => {
-    return msg;
-  });
-
-  if (!errors.isEmpty()) {
-    return res.status(422).json({
-      status: false,
-      message: errors.mapped()
-    });
-  }
-
-  let chatId = req.body.id;
-  const groupName = req.body.name;
-  const message = req.body.message;
-
-  // Find the group by name
-  if (!chatId) {
-    const group = await findGroupByName(groupName);
-    if (!group) {
-      return res.status(422).json({
-        status: false,
-        message: 'No group found with name: ' + groupName
-      });
-    }
-    chatId = group.id._serialized;
-  }
-
-  client.sendMessage(chatId, message).then(response => {
-    res.status(200).json({
-      status: true,
-      response: response
-    });
-  }).catch(err => {
-    res.status(500).json({
-      status: false,
-      response: err
-    });
-  });
-});
-
-// Clearing message on spesific chat
-app.post('/clear-message', [
-  body('number').notEmpty(),
-], async (req, res) => {
-  const errors = validationResult(req).formatWith(({
-    msg
-  }) => {
-    return msg;
-  });
-
-  if (!errors.isEmpty()) {
-    return res.status(422).json({
-      status: false,
-      message: errors.mapped()
-    });
-  }
-
-  const number = phoneNumberFormatter(req.body.number);
-
-  const isRegisteredNumber = await checkRegisteredNumber(number);
-
-  if (!isRegisteredNumber) {
-    return res.status(422).json({
-      status: false,
-      message: 'Esse numero não está registrado'
-    });
-  }
-
-  const chat = await client.getChatById(number);
-  
-  chat.clearMessages().then(status => {
-    res.status(200).json({
-      status: true,
-      response: status
-    });
-  }).catch(err => {
-    res.status(500).json({
-      status: false,
-      response: err
-    });
-  })
-});
-
-server.listen(port, function() {
-  console.log('Api Online na porta: ' + port);
-});
+server.keepAliveTimeout = 6000000;
