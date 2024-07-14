fc2fc49295a84ef5ccad7a4924697b1eb6094268
Katumbela <ja3328173@gmail.com>
2024-07-14 17:24:27 +0100

    fix sessions in mulple sessions all code

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,13 @@
 module.exports = {
-    apps : [{
-      name: "crm-360 wwebjs",
-      script: "app-multiple-account.js",
-      max_memory_restart: '1G',
-      cron_restart: '0 */24 * * *',
-      watch: true,
-      ignore_watch : ["node_modules"],
-      env: {
-        PORT: 8000
-      }
-    }]
-  }
-  
+  apps: [{
+    name: "crm-360 wwebjs",
+    script: "app.js",
+    max_memory_restart: '1G',
+    cron_restart: '0 */24 * * *',
+    watch: true,
+    ignore_watch: ["node_modules"],
+    env: {
+      PORT: 8000
+    }
+  }]
+}
